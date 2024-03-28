c12119c19d58e8d84f65bb6c4a361dffb7dfdb62
virani-077 <vishal07.360design@gmail.com>
2024-03-28 19:24:55 +0530

    dark

--- src/Component/Alert.js
+++ src/Component/Alert.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Alert = (props) => {
   return (
-    <div style={{ height: "50px", margin:"auto" }}>
+    <div style={{ height: "50px", margin:"auto", width:"24%" }}>
       {props.alert && (
         <div className="alert alert-success" role="alert">
           <strong>{props.alert.type}</strong> : {props.alert.msg}
